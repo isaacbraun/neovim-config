b18413e1a63fe7b49c81546a3d14e4689856ba3e
Isaac Braun <isaacbraun2000@gmail.com>
2024-03-22 16:04:05 -0700

    activated snippets completion

--- lua/isaacbraun/plugins/lsp.lua
+++ lua/isaacbraun/plugins/lsp.lua
@@ -56,6 +56,11 @@ return {
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
