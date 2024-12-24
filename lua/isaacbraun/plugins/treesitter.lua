14300b9d28f6e403b48821a58ab1a7df8cb177b9
Isaac Braun <isaacbraun2000@gmail.com>
2024-12-24 00:01:22 -0500

    change: formatting and changing lang opts

--- lua/isaacbraun/plugins/treesitter.lua
+++ lua/isaacbraun/plugins/treesitter.lua
@@ -1,16 +1,29 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      
-      config.setup({
-	ensure_installed = { "lua", "javascript", "html", "css", "python", "astro", "typescript", "jsdoc", "json" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"lua",
+					"javascript",
+					"html",
+					"css",
+					"python",
+					"astro",
+					"typescript",
+					"jsdoc",
+					"json",
+					"go",
+					"gomod",
+					"gowork",
+					"gosum",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
