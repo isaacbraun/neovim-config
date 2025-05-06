ece1bf71c5431e287ce80581a9e2eef3c67b3f91
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-05 23:52:27 -0700

    chore: formatting

--- lua/isaacbraun/plugins/none_ls.lua
+++ lua/isaacbraun/plugins/none_ls.lua
@@ -1,17 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.code_actions.gomodifytags,
-				null_ls.builtins.code_actions.impl,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.formatting.gofumpt,
-			},
-		})
-	end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.pint,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.code_actions.gomodifytags,
+        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.gofumpt,
+      },
+    })
+  end,
 }
