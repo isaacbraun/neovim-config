a77c1ca940ad65cfcaada02897d77b1b673c39dc
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-28 14:03:51 -0700

    feat: add non-ls-extras and require eslint_d

--- lua/isaacbraun/plugins/none_ls.lua
+++ lua/isaacbraun/plugins/none_ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
@@ -8,10 +11,17 @@ return {
         null_ls.builtins.formatting.pint,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.stylelint,
+        null_ls.builtins.code_actions.gomodifytags,
+        null_ls.builtins.code_actions.impl,
+
+        -- Formatters based-off the new none-ls-extras plugin
+        -- https://github.com/citosid/vim/blob/3dbe530186451342973be0fddb6e2e10099f1240/lua/plugins/none-ls.lua
+				require("none-ls.code_actions.eslint_d"),
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.formatting.eslint_d"),
       },
     })
   end,
