ae02ed1a7c8ff889ce26a9400864522865b1a1e6
Isaac Braun <isaacbraun2000@gmail.com>
2025-06-16 12:51:20 -0700

    feat(supermaven): disable supermaven for esri

--- lua/isaacbraun/plugins/supermaven.lua
+++ lua/isaacbraun/plugins/supermaven.lua
@@ -12,12 +12,23 @@ return {
       --   suggestion_color = "#ffffff",
       --   cterm = 244,
       -- },
-      log_level = "info",            -- set to "off" to disable logging completely
+      log_level = "info",             -- set to "off" to disable logging completely
       disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false,       -- disables built in keymaps for more manual control
+      disable_keymaps = false,        -- disables built in keymaps for more manual control
       condition = function()
-        return false
-      end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+        local cwd = vim.loop.cwd()
+        local blocked_dirs = {
+          "/Users/isa14596/esri",
+        }
+
+        for _, dir in ipairs(blocked_dirs) do
+          if cwd:find(dir, 1, true) == 1 then
+            return true -- stop supermaven
+          end
+        end
+
+        return false -- allow supermaven
+      end,
     })
   end,
 }
