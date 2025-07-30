fa47527c29e5e41c51c7ffc483b9992c42ad2f4d
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-30 13:04:34 -0700

    only run eslint when config file found

--- lua/isaacbraun/plugins/none_ls.lua
+++ lua/isaacbraun/plugins/none_ls.lua
@@ -6,23 +6,57 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.pint,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.stylelint,
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
+    -- Function to check for ESLint config in project root
+    local function has_eslint_config()
+      local configs = {
+        ".eslintrc",
+        ".eslintrc.js",
+        ".eslintrc.json",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.cjs",
+        "eslint.config.js",
+        "eslint.config.mjs",
+        "eslint.config.cjs",
+      }
 
-        -- Formatters based-off the new none-ls-extras plugin
-        -- https://github.com/citosid/vim/blob/3dbe530186451342973be0fddb6e2e10099f1240/lua/plugins/none-ls.lua
-				require("none-ls.code_actions.eslint_d"),
-        require("none-ls.diagnostics.eslint_d"),
-        require("none-ls.formatting.eslint_d"),
-      },
+      local cwd = vim.fn.getcwd()
+      for _, config in ipairs(configs) do
+        local path = cwd .. "/" .. config
+        if vim.fn.filereadable(path) == 1 then
+          if config == "package.json" then
+            local package_json = vim.fn.readfile(path)
+            local content = table.concat(package_json, "\n")
+            if content:find('"eslintConfig"') then
+              return true
+            end
+          else
+            return true
+          end
+        end
+      end
+      return false
+    end
+
+    local sources = {
+      null_ls.builtins.formatting.pint,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.goimports,
+      null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.stylelint,
+      null_ls.builtins.code_actions.gomodifytags,
+      null_ls.builtins.code_actions.impl,
+    }
+
+    if has_eslint_config() then
+      table.insert(sources, null_ls.builtins.diagnostics.eslint_d)
+      table.insert(sources, null_ls.builtins.formatting.eslint_d)
+      table.insert(sources, null_ls.builtins.code_actions.eslint_d)
+    end
+
+    null_ls.setup({
+      sources = sources,
     })
   end,
 }
