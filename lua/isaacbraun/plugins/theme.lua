502b8e35516d2a2b2da839d8e53528593fa3a2c4
Isaac Braun <isaacbraun2000@gmail.com>
2026-04-03 18:40:48 -0700

    feat: add support for light and dark, change to catppuccin

--- lua/isaacbraun/plugins/theme.lua
+++ lua/isaacbraun/plugins/theme.lua
@@ -1,11 +1,23 @@
 return {
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,
+  --   name = "gruvbox",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
   {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    name = "gruvbox",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
+    background = {
+      light = "latte",
+      dark = "mocha",
+    },
     config = function()
-      vim.cmd.colorscheme("gruvbox")
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
