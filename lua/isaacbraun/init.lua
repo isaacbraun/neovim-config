9d298dc2742951e70c3f037a487ec1b0a4694f60
Isaac Braun <isaac@braunbauen.com>
2026-06-11 13:54:01 -0700

    chore(lazy): update lazy.vim setup to latest doc recommendation
    Also fixes a autocmd launch error

--- lua/isaacbraun/init.lua
+++ lua/isaacbraun/init.lua
@@ -1,40 +1,27 @@
 require("isaacbraun.remap")
 require("isaacbraun.set")
 
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local autocmd = vim.api.nvim_create_autocmd
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
--- Change filetype to gotmpl if go html detected (https://neovim.io/doc/user/lua.html#vim.filetype.add())
--- vim.filetype.add {
---   pattern = {
---     ['.html'] = {
---       function(path, bufnr)
---         local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
---         if vim.regex([[^#!.*\\<{{\\>]]):match_str(content) ~= nil then
---           return 'mine'
---         end
---       end,
---       { priority = -math.huge },
---     },
---   },
--- }
-
+-- Setup lazy.nvim
 require("lazy").setup("isaacbraun.plugins")
 
-autocmd('LspAttach', {
+vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -53,5 +40,3 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
-vim.o.termguicolors = true
