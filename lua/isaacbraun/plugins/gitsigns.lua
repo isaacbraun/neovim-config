0cef0b97a55bfdec7ef4bfb8937af5551d63f020
Isaac Braun <isaac@braunbauen.com>
2026-06-12 12:50:57 -0700

    feat(gitsigns): enabled word diff and add hunk keymaps

--- lua/isaacbraun/plugins/gitsigns.lua
+++ lua/isaacbraun/plugins/gitsigns.lua
@@ -3,8 +3,15 @@ return {
   config = function()
     require("gitsigns").setup({
       current_line_blame = true,
+      word_diff = true,
     })
 
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
+    vim.keymap.set("n", "<leader>sh", ":Gitsigns stage_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>rh", ":Gitsigns reset_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>sb", ":Gitsigns stage_buffer<CR>", {})
+    vim.keymap.set("n", "<leader>rb", ":Gitsigns reset_buffer<CR>", {})
+    vim.keymap.set("n", "<leader>nh", ":Gitsigns next_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>ph", ":Gitsigns prev_hunk<CR>", {})
   end
 }
