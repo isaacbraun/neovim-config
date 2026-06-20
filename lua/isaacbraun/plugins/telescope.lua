cbe5008882a2efcbab396b8d603546613cfcaa88
Isaac Braun <isaac@braunbauen.com>
2026-06-19 18:07:24 -0700

    feat(telescope): add keybinds for searching symbols and types
    This also changes the replace-all keybind from <leader>s to <leader>r

--- lua/isaacbraun/plugins/telescope.lua
+++ lua/isaacbraun/plugins/telescope.lua
@@ -32,6 +32,10 @@ return {
           prompt_title = "Grep in .github Files",
         })
       end)
+      vim.keymap.set("n", "<leader>s", builtin.lsp_document_symbols, {})
+      vim.keymap.set("n", "<leader>sw", builtin.lsp_workspace_symbols, {})
+      vim.keymap.set("n", "<leader>sr", builtin.lsp_references, {})
+      vim.keymap.set("n", "<leader>t", builtin.lsp_type_definitions, {})
     end,
   },
 }
